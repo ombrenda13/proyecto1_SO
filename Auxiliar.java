321481663855b448b230aba3bb35519182e566e5
ombrenda13 <36515687+ombrenda13@users.noreply.github.com>
2018-10-05 18:42:11 -0500

    Add files via upload

---
+++ Auxiliar.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author brenda
+ */
+public class Auxiliar {
+    
+    
+    
+    private int  id_proceso;
+	private String nombre;
+	private int  tamaño;
+	private int tiempo_ejecucion;
+	private int prioridad;
+	private int tiempo_es;
+	private int tiempo_llegada;
+
+        Proceso siguiente;
+        
+        
+	public   int getIdproceso(){
+		return id_proceso;
+	
+					}
+	public  void setIdproceso(int id_proceso){
+		this.id_proceso=id_proceso;
+             
+						}
+
+        public   String getNombre(){
+		return nombre;
+	
+					}
+	public  void setNombre(String nombre){
+		this.nombre=nombre;
+						}
+        
+         public int getTamaño(){
+		return tamaño;
+	
+					}
+	public  void setTamaño(int tamaño){
+		this.tamaño=tamaño;
+						}
+        
+         public int getTiempoej(){
+		return tiempo_ejecucion;
+	
+					}
+	public  void setTiempoej(int tiempo_ejecucion){
+		this.tiempo_ejecucion=tiempo_ejecucion;
+						}
+        
+         public int getPrioridad(){
+		return tiempo_ejecucion;
+	
+					}
+	public  void setPrioridad(int prioridad){
+		this.prioridad=prioridad;
+						}
+       
+        
+         public int getTiempoesp(){
+		return tiempo_ejecucion;
+	
+					}
+	public  void setTiempoesp(int tiempo_es){
+		this.tiempo_es=tiempo_es;
+						}
+        
+         public int getTiempolleg(){
+		return tiempo_ejecucion;
+	
+					}
+	public  void setTiempolleg(int tiempo_llegada){
+		this.tiempo_llegada=tiempo_llegada;
+						}
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
--- mainProyecto.java
+++ mainProyecto.java
@@ -3,6 +3,11 @@ import java.io.*;
 import java.util.*;
 
 public class mainProyecto {
+    
+    
+
+        
+                     
  	public static void main(String args[]){
 			int contador, contador2;
 			Scanner num= new Scanner(System.in);
@@ -15,8 +20,7 @@ public class mainProyecto {
                         int prioridad;
                         int tiempo_es;
                         int tiempo_llegada;
-			int j;
-			boolean bool=false;
+			
 
 				System.out.println("ESCRIBE EL NUMERO DE PROCESOS QUE DESEAS SIMULAR:");
 				int numeroprocesos= num.nextInt();
@@ -25,61 +29,63 @@ public class mainProyecto {
 				
 						 Proceso pro[] = new Proceso [numeroprocesos];
                                                  Nodo n[]=new Nodo [numeroprocesos];
+                                               
+                                                 
 					for (contador2=0;contador2<numeroprocesos;contador2++){
 						pro[contador2] = new Proceso(); //crea los arreglos de objetos de dentro de una posicion
                                                 
-                                                n[contador2]= new Nodo();//creacion de nodo por cada proceso
+//                                                n[contador2]= new Nodo();//creacion de nodo por cada proceso
                                                 
 						
                                                 System.out.println("id_proceso:"+(contador2+1));
-                                               
-                                                System.out.println("nombre del proceso:\n");
-                                                nombre=leer.nextLine();
-                                                
-                                                System.out.println("tamaño del proceso:\n");
-                                                tamaño=leer.nextInt();                 
-                                                
-                                                System.out.println("tiempo de ejecucion[ms]:\n");
-                                                tiempo_ejecucion=leer.nextInt();
-                                                
-                                                System.out.println("prioridad del proceso:\n");
-                                                prioridad=leer.nextInt();
-                                                
-                                                System.out.println("tiempo de espera[ms]:\n");
-                                                tiempo_es=leer.nextInt();
-                                                
+                                                pro[contador2].setIdproceso(contador2+1);
+//                                                System.out.println("nombre del proceso:\n");
+//                                                nombre=leer.nextLine();
+//                                                
+//                                                System.out.println("tamaño del proceso:\n");
+//                                                tamaño=leer.nextInt();                 
+//                                                
+//                                                System.out.println("tiempo de ejecucion[ms]:\n");
+//                                                tiempo_ejecucion=leer.nextInt();
+//                                                
+//                                                System.out.println("prioridad del proceso:\n");
+//                                                prioridad=leer.nextInt();
+//                                                
+//                                                System.out.println("tiempo de espera[ms]:\n");
+//                                                tiempo_es=leer.nextInt();
+//                                                
                                                 System.out.println("tiempo de llegada[ms]:\n");
                                                 tiempo_llegada=leer.nextInt();
 						
                                                 
-                                                pro[contador2].setIdproceso(contador2+1);
-                                                pro[contador2].setNombre(nombre);
                                                
-                                                pro[contador2].setNombre(nombre);
-                                                pro[contador2].setTamaño(tamaño);
-                                              
-                                                pro[contador2].setPrioridad(prioridad);
-                                                pro[contador2].setTiempoej(tiempo_ejecucion);
-                                                pro[contador2].setTiempoesp(tiempo_es);
-                                                pro[contador2].setTiempolleg(tiempo_llegada);
+//                                                pro[contador2].setNombre(nombre);
+//                                               
+//                                                pro[contador2].setNombre(nombre);
+//                                                pro[contador2].setTamaño(tamaño);
+//                                              
+//                                                pro[contador2].setPrioridad(prioridad);
+//                                                pro[contador2].setTiempoej(tiempo_ejecucion);
+//                                                pro[contador2].setTiempoesp(tiempo_es);
+                                                
                                                         leer.nextLine();
-                                        
+                                        pro[contador2].setTiempolleg(tiempo_llegada);
                                         }
 				
-                                        System.out.println("\nLOS DATOS DE TUS PROCESOS SON:\n");
-					for(int i=0;i<numeroprocesos;i++)
+                                      	for(int i=0;i<numeroprocesos;i++)
                                                 {
                                         
 					System.out.println("ID_PROCESO:"+pro[i].getIdproceso());
-                                        System.out.println("NOMBRE:"+pro[i].getNombre());
-                                        System.out.println("TAMAÑO:"+pro[i].getTamaño());
-                                        System.out.println("PRIORIDAD:"+pro[i].getPrioridad());
-                                        System.out.println("TIEMPO DE EJECUCION:"+pro[i].getTiempoej()+"ms");
-                                        System.out.println("TIEMPO DE ESPERA:"+pro[i].getTiempoesp()+"ms");
+//                                        System.out.println("NOMBRE:"+pro[i].getNombre());
+//                                        System.out.println("TAMAÑO:"+pro[i].getTamaño());
+//                                        System.out.println("PRIORIDAD:"+pro[i].getPrioridad());
+//                                        System.out.println("TIEMPO DE EJECUCION:"+pro[i].getTiempoej()+"ms");
+//                                        System.out.println("TIEMPO DE ESPERA:"+pro[i].getTiempoesp()+"ms");
                                         System.out.println("TIEMPO DE LLEGADA:"+pro[i].getTiempolleg()+"ms");
                                         System.out.println("-------------------------");
-                                        }			
-                                        
+} 
+                                        System.out.println("HOLA:"+pro[0].getTiempolleg());
+                                      System.out.println("PRUEBAAAA:"+crearCola(pro)); //enlaza los nodos 
                                         
                                         
                                         
@@ -88,12 +94,33 @@ public class mainProyecto {
 
                                  
         
-                                        
+      public static Proceso crearCola(Proceso[] pro){
+             Auxiliar aux[]= new Auxiliar[0];
+            System.out.println("prueba:"+pro[0].getTiempolleg());
+//          for (int i=0; i<numeroprocesos; i++){
+//             
+//              for (int j=1; j<numeroprocesos;j++){
+//                         if(pro[i].getTiempolleg()<pro[j].getTiempolleg()){
+//                                aux[0]=new Auxiliar();
+//                                aux[0].setTiempolleg(pro[j].getTiempolleg());
+//                                
+//                                pro[j].setTiempolleg(pro[i].getTiempolleg());
+//                                pro[j].setTiempolleg(aux[0].getTiempolleg());
+//                               
+//                         }
+//              
+//              
+//             }
+//        
+//        
+//          }
+          
+          
+          
         
-        
-        
+        return pro[0];
 }
-
+}
         
         
         
